cf2cfab0fb7d4309550c95e0e64e226f0a1f7235
akshaynaganwar1996 <akshaynaganwar1996@gmail.com>
2024-12-12 20:08:03 -0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componets/navbar/navbar.component';
+import { HomeComponent } from './componets/home/home.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { EmailComponent } from './componets/email/email.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+import{MatInputModule} from '@angular/material/input';
+
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    EmailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
+    
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync(),
+    MatSnackBar
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
